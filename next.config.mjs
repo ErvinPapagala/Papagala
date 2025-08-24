6b5aa4f0ab6163cb918149ba95cb549c3b9d7f26
Ervin Papagala <epapagala_al@outlook.com>
2025-08-24 19:12:19 +0300

    Fix another CSS syntax error - remove stray forward slash on line 602

--- next.config.mjs
+++ next.config.mjs
@@ -4,20 +4,6 @@ import images from './next.config.images.mjs';
 const nextConfig = {
   reactStrictMode: true,
   ...images,
-  // Disable CSS optimization to fix build issues
-  experimental: {
-    optimizeCss: false,
-  },
-  // Alternative approach - configure webpack to handle CSS differently
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      // Disable CSS minification that's causing issues
-      config.optimization.minimizer = config.optimization.minimizer.filter(
-        (plugin) => plugin.constructor.name !== 'CssMinimizerPlugin'
-      );
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
